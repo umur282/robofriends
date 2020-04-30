d050f90ffea0c87b85fd98f5929b2cf2793adce4
umur282 <umur.282@gmail.com>
2020-04-30 15:38:17 +0200

    Updates

---
+++ precache-manifest.ec9b16794e30169c4012093ec4b73943.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e3773d6c6c990434fb0649a7a469fe",
+    "url": "/robofriends-app/index.html"
+  },
+  {
+    "revision": "f7a543b88f33828d3de6",
+    "url": "/robofriends-app/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "9154c023d9c4320abec4",
+    "url": "/robofriends-app/static/css/main.38a7e89b.chunk.css"
+  },
+  {
+    "revision": "f7a543b88f33828d3de6",
+    "url": "/robofriends-app/static/js/2.c1100ffe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends-app/static/js/2.c1100ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9154c023d9c4320abec4",
+    "url": "/robofriends-app/static/js/main.296c9891.chunk.js"
+  },
+  {
+    "revision": "1a4221f37428196e29c2",
+    "url": "/robofriends-app/static/js/runtime-main.47f3165e.js"
+  },
+  {
+    "revision": "692c37377cd1e0f4850f47c7c0148426",
+    "url": "/robofriends-app/static/media/VT323-Regular.692c3737.ttf"
+  }
+]);
